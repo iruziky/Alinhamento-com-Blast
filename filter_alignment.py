e2b9ff39f9f8c9a7fa248fa56256b84448b5f0fa
iruziky <112575824+iruziky@users.noreply.github.com>
2024-05-23 17:15:36 -0300

    Update filter_alignment.py

--- filter_alignment.py
+++ filter_alignment.py
@@ -1,15 +1,10 @@
 import os
 import pandas as pd
 
-result_dir = "/home/iruziky/resultados"
+result_dir = "\\Users\\Iruziky\\Downloads\\mtDNAxNCBI"
 
 fileNames_list = os.listdir(result_dir)
 
-# col_names = [
-#     'slen', 'qstart', 'qend', 'sstart', 
-#     'send', 'bitscore', 'score', 'length', 'pident', 'nident', 'mismatch', 
-#     'positive', 'gapopen', 'gaps', 'ppos'
-# ]
 uniResults = []
 for fileName in fileNames_list:
     file_path = os.path.join(result_dir, fileName)  
@@ -17,22 +12,16 @@ for fileName in fileNames_list:
     with open(file_path, 'r') as searchResult:
         file = searchResult.read()
         lines = file.split('\n')    
-
-    # resultLines = []
-    # for i in range (10):
-    #     line = lines[i].split(',')
-    #     resultLines.append(line[12:27])
     
-    #df = pd.DataFrame(data=resultLines, columns=col_names)
+    try:
+        line = lines[0].split(',')
+        line[11] = fileName
+        uniResults.append(line[11:27])
 
-    # Salvando o DataFrame como um arquivo CSV
-    #csv_file_path = f"/home/iruziky/alignment/{fileName}.csv"  # Definindo o caminho para salvar o CSV
-    #df.to_csv(csv_file_path, index=False)  # index=False para não salvar o índice do DataFrame
-
-    
-    line = lines[0].split(',')
-    line[11] = fileName
-    uniResults.append(line[11:27])
+    except:
+        print("Erro ao filtrar os dados do arquivo",fileName,
+              "\n Este arquivo será desconsiderado")
+        continue        
 
 col_names = [
     'specie', 'slen', 'qstart', 'qend', 'sstart', 
@@ -43,5 +32,5 @@ col_names = [
 df = pd.DataFrame(data=uniResults, columns=col_names)
 
 # Salvando o DataFrame como um arquivo CSV
-csv_file_path = f"/home/iruziky/alignment/geral.csv"  # Definindo o caminho para salvar o CSV
+csv_file_path = f"\\Users\\Iruziky\\Desktop\\geral.csv"  # Definindo o caminho para salvar o CSV
 df.to_csv(csv_file_path, index=False)  # index=False para não salvar o índice do DataFrame
